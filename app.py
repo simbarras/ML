2b67e34f71db6fe269278d2e90921d943e1dff55
Simon Barras <66463606+simbarras@users.noreply.github.com>
2021-04-20 18:32:08 +0200

    add normal vector

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ from ML.lr.lr_simple import LrSimple
 
 if __name__ == '__main__':
     # lr = LrSimple()
-    #lr = LrMultiple()
+    # lr = LrMultiple()
     lr = Lr()
 
     lr.learning_rate = 0.01
-    lr.n_iteration = 100
+    lr.n_iteration = 400
     lr.n_samples = 100
-    lr.n_features = 1
     lr.n_param_more = 0
     lr.verbose = True
 
-    lr.randomDataset()
-    lr.run()  # Run the regression
+    lr.n_features = 1
+    lr.random_dataset(10)
+
+    lr.run("Grad")  # Run the regression
+
+    lr.use_normal_vector = True
+    lr.run("Normal")  # Run the regression
--- lr/lr.py
+++ lr/lr.py
@@ -12,42 +12,47 @@ class Lr:
     n_samples = 100
 
     # Configuration
-    n_features = 1
     n_param_more = 0
-
+    use_normal_vector = False
     verbose = False
 
     # dataset
     x = None
     y = None
+    n_features = 1
 
-    def run(self):  # return the coef and the final theta
+    def run(self, name=""):  # return the coef and the final theta
         self.y = self.y.reshape(self.y.shape[0], 1)
 
         X = self.make_X(self.n_features + self.n_param_more)  # X -> arguments input [[a, 1],[b, 1]...]
 
-        theta = np.random.randn(X.shape[1], 1)  # Matrix with the arguments of the function
+        if not self.use_normal_vector:
 
-        # Learn
-        theta_final, cost_history = self.gradient_descent(X, self.y, theta, self.learning_rate, self.n_iteration)
+            theta = np.random.randn(X.shape[1], 1)  # Matrix with the arguments of the function
+
+            # Learn
+            theta_final, cost_history = self.gradient_descent(X, self.y, theta, self.learning_rate, self.n_iteration)
+        else:
+            theta_final = self.normal_vector(self.X, self.y)
+
+        predictions = self.model(X, theta_final)  # Make predictions
+        coef = self.coef_determination(self.y, predictions)  # Compute the coef / 1
 
         if self.verbose:
-            predictions = self.model(X, theta_final)
-
-            print("\nStats: Linear regression")  # Show Stats
-            print("Iteration: " + str(self.n_iteration))
-            print("Learning rate: " + str(self.learning_rate))
+            print("\nStats: Linear regression ({})".format(name))  # Show Stats
+            print("Use normal vector: " + str(self.use_normal_vector))
             print("Samples: " + str(self.n_samples))
             print("Features: " + str(self.n_features))
             print("More parameters: " + str(self.n_param_more))
+            if not self.use_normal_vector:
+                print("Iteration: " + str(self.n_iteration))
+                print("Learning rate: " + str(self.learning_rate))
             print("y: " + str(self.y.shape))
             print("x: " + str(self.x.shape))
-            print("X: " + str(X.shape))
-            print("Theta: " + str(theta.shape))
+            print("Theta: " + str(theta_final.shape))
             print("Theta final: " + str(theta_final))
 
-            # Show coef /1
-            coef = self.coef_determination(self.y, predictions)
+            # Show coef
             print("Coef: {0:9.3f}/1 ({0})".format(coef))
 
             # Show graphs
@@ -57,12 +62,15 @@ class Lr:
                 plt.show()
 
             #  Show cost evolution
-            plt.plot(range(self.n_iteration), cost_history)
-            plt.show()
+            if not self.use_normal_vector:
+                plt.plot(range(self.n_iteration), cost_history)
+                plt.show()
 
         return theta_final, coef
 
-    """ ML FUNCTION """
+    """=================================================================================================================
+    ¦¦¦ Machine Learning FUNCTION                                                                                    ¦¦¦
+    ================================================================================================================="""
 
     def model(self, X, theta):
         return X.dot(theta)
@@ -82,21 +90,26 @@ class Lr:
             cost_history[i] = self.cost(X, y, theta)
         return theta, cost_history
 
+    def normal_vector(self, x, y):
+        return np.linalg.inv(x.T.dot(x)).dot((x.T.dot(y)))
+
     def coef_determination(self, y, pred):
         u = ((y - pred) ** 2).sum()
         v = ((y - y.mean()) ** 2).sum()
         return 1 - u / v
 
-    """ CONSTRUCTION FUNCTION """
-
-    def randomDataset(self):
-        self.x, self.y = make_regression(n_samples=self.n_samples, n_features=self.n_features,
-                                         noise=10)  # x -> inputs  y -> outputs
-        self.y = self.y.reshape(self.y.shape[0], 1)
-        return self.x, self.y
+    """=================================================================================================================
+    ¦¦¦ CONSTRUCTION FUNCTION                                                                                        ¦¦¦
+    ================================================================================================================="""
 
     def make_X(self, n_param):
         if n_param <= 2:
             return np.hstack((self.x, np.ones((self.x.shape[0], 1))))
         else:
             return np.hstack((self.x ** (n_param - 1), self.make_X(n_param - 1)))
+
+    def random_dataset(self, noise):
+        self.x, self.y = make_regression(n_samples=self.n_samples, n_features=self.n_features,
+                                         noise=noise)  # x -> inputs  y -> outputs
+        self.y = self.y.reshape(self.y.shape[0], 1)
+        return self.x, self.y
