8b18476821bbdba36f50346843dd5955a49dbb4e
Simon Barras <simon.barras02@gmail.com>
2021-09-28 23:07:02 +0200

    add test

---
+++ test/test_lr.py
@@ -0,0 +1,19 @@
+import lr.lr as regressor
+
+def test_random():
+    lr = Lr()
+
+    lr.learning_rate = 0.0001
+    lr.n_iteration = 1000
+    lr.n_samples = 5
+    lr.n_param_more = 2
+    lr.verbose = True
+
+    lr.n_features = 1
+
+    
+    lr.random_dataset(10)
+
+    lr.run("Grad")  # Run the regression
+
+    lr.run("Force theta")  # Run the regression
